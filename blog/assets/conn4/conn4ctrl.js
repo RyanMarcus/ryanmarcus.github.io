5a8fcdbdded5558fdd2be2e1527c49794fac6d6b
Ryan Marcus <ryan@rmarcus.info>
2015-09-03 14:24:35 -0400

    reverting broken changes

--- blog/assets/conn4/conn4ctrl.js
+++ blog/assets/conn4/conn4ctrl.js
@@ -20,51 +20,51 @@
 */
 
 
-var conn4App = angular.module('conn4App', []);
 
-conn4App.controller('ConnFourCtrl', ['$scope', function($scope) {
+function ConnFourCtrl($scope) {
+
 	$scope.worker = new Worker('/blog/assets/conn4/conn4worker.js');
-	
-	
+
+
 	$scope.board = [];
 	$scope.look_ahead = 5;
 	$scope.winner = false;
 	$scope.draw = false;
 	$scope.thinking = false;
 	$scope.isOver = {};
-	
+
 	$scope.move = function(i) {
 		if ($scope.thinking)
 			return;
-		
+
 		if ($scope.winner)
 			return;
 		$scope.thinking = true;
 		$scope.worker.postMessage({'cmd': 'move', 'val': i});	
 	};
-	
-	
+
+
 	$scope.$watch(function() { return $scope.look_ahead; }, function() {
 		$scope.worker.postMessage({'cmd': "ahead", 'val': $scope.look_ahead});
 	});
-	
+
 	$scope.worker.postMessage({'cmd': 'new'});
 	$scope.worker.addEventListener('message', function(data) {
 		var m = data.data;
 		$scope.board = m['board'];
-		
+
 		$scope.winner = m['win'];
 		$scope.draw = m['draw'];
 		$scope.thinking = m['thinking'];
 		$scope.$apply();
 	});
-	
+
 	$scope.over = function(idx) {
 		$scope.isOver[idx] = true;
 	};
-	
+
 	$scope.leave = function(idx) {
 		$scope.isOver[idx] = false;
 	};
-	
-}]);
+
+}
